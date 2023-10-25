9f7bda97ba560c1c26eef87b3404623279be74c8
YaslePoy <mmm606@inbox.ru>
2023-10-25 21:26:31 +0300

    adding ChunkLocation

---
+++ Mega/Game/ChunkLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega.Game
+{
+    internal class ChunkLocation
+    {
+        public byte X, Z;
+        public ushort Y;
+    }
+}
