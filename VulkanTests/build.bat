ed464fef8d961d9b290179c506c121d89a7ca47d
YaslePoy <mmm606@inbox.ru>
2023-11-14 21:03:59 +0300

    updated to .net 8

--- VulkanTests/build.bat
+++ VulkanTests/build.bat
@@ -1,8 +1,12 @@
 echo on
 echo "compile&copy"
 echo %1
-if not exist %1 mkdir %1
-glslc shader.vert -o %1\vert.spv
+
+if not exist %1\shaders mkdir %1\shaders
+glslc shader.vert -o %1\shaders\vert.spv
 echo "vert compiled"
-glslc shader.frag -o %1\frag.spv
+glslc shader.frag -o %1\shaders\frag.spv
 echo "frag compiled"
+
+if not exist %1\textures mkdir %1\textures
+copy texture.png %1\textures
